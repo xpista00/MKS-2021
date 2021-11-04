19b1f5ada2b6cb8ed763e6918878c02ba42ef57f
Ondrej Pistak <ondra.pistak@volny.cz>
2021-11-04 09:04:08 +0100

    cv6_ukol2_a_grafy

---
+++ cv6_graf.m
@@ -0,0 +1,43 @@
+
+clear all
+close all
+clc
+
+databaze = "ntc.csv";
+data=csvread(databaze);
+
+
+U_NTC= 3.3 .* data(:,2)./(10 + data(:,2));
+
+ad= 1024*U_NTC/3.3;
+
+t=data(:,1);
+
+p=polyfit(ad,t,10);
+
+ad2=0:1023
+
+t2 = round(polyval(p, ad2), 1); 
+
+figure(1)
+hold on
+plot(ad,data(:,1),'o')
+plot(ad2, t2, 'r')
+
+
+dlmwrite('data.dlm', t2*10, ',');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
